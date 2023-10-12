4c7bad2b5e4cd8ce96749a1e237c6d2f910bf63b
DinaEllinson <Dina@gmail.com>
2023-10-12 15:50:23 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program9181.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+
+namespace stage0
+{
+    partial class program
+    {
+        static void Main(String[] args)
+        {
+            Welcome9181();
+            Welcome1388();
+            Console.ReadKey();
+        }
+        static partial void Welcome1388();
+        private static void Welcome9181()
+        {
+            Console.Write("Enter your name:");
+            var a = Console.ReadLine();
+            Console.WriteLine($"{a}, welcome to my first console application");
+        }
+    }
+}
