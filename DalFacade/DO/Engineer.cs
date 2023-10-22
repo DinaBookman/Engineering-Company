105d02f1d76eed626c0346d457003a7212ecc920
DinaEllinson <Dina@gmail.com>
2023-10-22 17:03:53 +0300

    push

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,15 +1,20 @@
 ﻿namespace DO;
+/// <summary>
+/// Student Entity represents an engineer with all its props
+/// </summary>
+/// <param name="Id">Personal unique ID of engineer (as in national id card)</param>
+/// <param name="Name">engineer's name</param>
+/// <param name="Email">engineer's email adrres</param>
+/// <param name="Level">engineer's level can either be a manager or engineer</param>
+/// <param name="Wage">salary per hour</param>
 public record Engineer
 (
    int Id,
-   string EngineerName,
-
-
-
+   string Name,
+   string Email,
+   EngineerExperience Level,
+   int Wage
 );
+ 
 
-מספר מזהה ייחודי
-שם המהנדס
-כתובת דוא"ל
-רמת המהנדס
-עלות לשעה
+ 
--- DalFacade/DO/Task.cs
+++ DalFacade/DO/Task.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DO
+﻿namespace DO;
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Id">unique ID (created automatically)</param>
+/// <param name="Description">describe the task</param>
+/// <param name="Alias">The alias of the task</param>
+/// <param name="StartDate">the real start date</param>
+/// <param name="ScheduledDate">the original date on which it should be completed</param>
+/// <param name="ForecastDate">a revised scheduled completion date</param>
+/// <param name="EngineerId"></param>
+/// <param name="CopmlexityLevel">task: minimum expirience for engineer to assign</param>
+/// <param name="IsMilestone"></param>
+public record Task
+(
+    int Id,
+    string Description,
+    string Alias,
+    DateTime StartDate,
+    DateTime ScheduledDate,
+    DateTime ForecastDate,
+    int EngineerId,
+    EngineerExperience CopmlexityLevel,
+    bool IsMilestone = false
+)
 {
-    internal class Task
-    {
-    }
+    DateTime CreatedAtDate => DateTime.Now;//ate when the task was added to the system, get only
+    DateTime CompleteDate;//real completion date.
+    string Deliverables;//description of deliverables for MS copmletion.
+    string Remarks;//free remarks from project meetings.
 }
