e282dbaf765c872d7c02f5046cc13f67c87b09a7
אלינסון דינה < @B27>
2023-11-07 15:42:49 +0200

    DakTest fix

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -8,7 +8,10 @@ namespace DalTest
 {
     internal class Program
     {
-        static readonly IDal s_dal = new DalList(); //stage 2
+        static readonly IDal? s_dal = new Dal.DalList(); //stage 2
+       
+
+        
 
         //function to create an engineer.
         private static Engineer CreateEngineer()
