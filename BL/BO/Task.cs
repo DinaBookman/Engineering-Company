966446d4f4dd29e0778db0952b1eb015eb48ff5e
ShiraPliskin <shirap803@gmail.com>
2023-12-11 23:03:26 +0200

    logic entities

--- BL/BO/Task.cs
+++ BL/BO/Task.cs
@@ -1,12 +1,4 @@
 ﻿namespace BO;
-/// <summary>
-/// 
-/// <param name="Id">Personal unique ID of engineer (as in national id card)</param>
-/// <param name="Name">engineer's name</param>
-/// <param name="Email">engineer's email address</param>
-/// <param name="Level">engineer's level can either be a manager or engineer</param>
-/// <param name="Cost">salary per hour</param>
-/// </summary>
 public class Task
 {
     public int Id { get; init; }
@@ -14,8 +6,8 @@ public class Task
     public string? Alias { get; set; }
     public DateTime CreatedAtDate { get; init; }
     public Status Status { get; set; }
-    public TaskInList DependenciesList { get; set; }
-    public MilestoneInTask Milestone { get; set; }
+    public TaskInList? DependenciesList { get; set; }
+    public MilestoneInTask? Milestone { get; set; }
     public DateTime ScheduledStartDate { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime? ForecastAtDate { get; set; }
