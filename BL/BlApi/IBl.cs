b405a08ba9eab3f639188135a8f748cdd73e3446
DinaEllinson <Dina@gmail.com>
2024-01-03 21:26:23 +0200

    Stage 5 chapter 5 try to run

--- BL/BlApi/IBl.cs
+++ BL/BlApi/IBl.cs
@@ -3,5 +3,5 @@ public interface IBl
 {
     public IEngineer Engineer { get; }
     public ITask Task { get; }
-    public IMilestone Milestone { get; }
+    //public IMilestone Milestone { get; }
 }
